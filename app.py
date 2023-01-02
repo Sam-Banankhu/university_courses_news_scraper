9f1d916ea1b3ad69548de9b418470aa4dbd97c49
Samson Banankhu Mhango <sambanankhumhango@gmail.com>
2023-01-02 16:28:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,18 @@ from bs4 import BeautifulSoup
 import requests
 from time import sleep
 
+
+# functions to get the link of a website and return souped content
 def get_link(url):
     re = requests.get(url)
     soup = BeautifulSoup(re.content, 'html.parser')
     
     return soup
+
+
+# functions to get the beautfied content of a website and return divs containers for courses
+def get_soup(soup):
+    results = soup.find('div', class_ = 'row vacancy-container')
+    divs = results.find_all('div', {'class':'col-sm-6'})
+    
+    return divs
