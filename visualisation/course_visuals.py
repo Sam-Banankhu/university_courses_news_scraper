b7181f375645611062f1346f1736b084d64631db
Samson Banankhu Mhango <sambanankhumhango@gmail.com>
2023-01-02 17:54:29 +0200

    added some courses visuals

--- visualisation/course_visuals.py
+++ visualisation/course_visuals.py
@@ -12,4 +12,29 @@ sys.path.insert(0, '/home/pandas/Desktop/scraper/')
 # reading the data
 df = pd.read_csv('courses.csv')
 
-print(df.head())
+
+
+plt.barh(df['faculty'].value_counts().keys(), df['faculty'].value_counts())
+plt.title("GRAPH SHOWING FACULTIES AND THEIR PROGRAMS")
+plt.ylabel('Faculties')
+plt.xlabel('Number of programs')
+plt.savefig('fig1.png')
+plt.show()
+
+
+
+sns.barplot(data = df, y = df['faculty'].value_counts().keys(), x = df['faculty'].value_counts())
+plt.title("GRAPH SHOWING FACULTIES AND THEIR PROGRAMS")
+plt.ylabel('Faculties')
+plt.xlabel('Number of programs')
+plt.savefig('fig2.png')
+plt.show()
+# print(df.head())
+
+
+sns.barplot(data = df, x = 'faculty', y = 'duration', hue="level")
+plt.title('GRAPHICS')
+plt.ylabel('Faculties')
+plt.xlabel('Number of programs')
+plt.savefig('fig3.png')
+plt.show()
