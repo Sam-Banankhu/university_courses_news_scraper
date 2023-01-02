9ecf94c0d04f61f2ea8afa2dc99670b28d593ba4
Samson Banankhu Mhango <sambanankhumhango@gmail.com>
2023-01-02 16:30:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,17 @@ def get_link(url):
 
 
 # functions to get the beautfied content of a website and return divs containers for courses
-def get_soup(soup):
+def get_souped_courses(soup):
     results = soup.find('div', class_ = 'row vacancy-container')
     divs = results.find_all('div', {'class':'col-sm-6'})
     
     return divs
+
+
+# functions to get the beautfied news content of a website and return article, date, author 
+def get_souped_news(news):
+    author = news.find('div', class_='news-content').text.replace('\n','').replace('\xa0','')
+    date = news.find('div', {"class":"news-date"}).text.split("\t")[8].split('\n')[0].strip()
+    article = news.find('div', {"class":"news-date"}).text.split("\t")[4].split('\xa0')[0].strip()
+    
+    return article, date, author 
